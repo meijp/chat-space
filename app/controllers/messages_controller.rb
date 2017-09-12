e32924ca2d21f69bd998ce42ca789baec4281643
meijp <mai@mais-MacBook-Pro.local>
2017-09-12 19:28:11 +0900

    Create Signup function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,8 @@
 class MessagesController < ApplicationController
 
-  before_action :move_to_index, except: :index
+  before_action :authenticate_member!
 
   def index
   end
 
-  def move_to_index
-      redirect_to action: :index unless user_signed_in?
-  end
 end
