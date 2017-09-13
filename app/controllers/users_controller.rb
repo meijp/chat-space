d3c716b030311982131d065e5e47cb6c2104148b
meijp <mai@mais-MacBook-Pro.local>
2017-09-13 18:40:33 +0900

    Create user-edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
 
   def edit
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(current_user.id)
     if @user.update(user_params)
       redirect_to root_path
+    else
+      render :edit
     end
   end
 
