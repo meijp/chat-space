e8e63faf5d6ca41531d75fdb0bdbea739ecf5eb3
meijp <mai@mais-MacBook-Pro.local>
2017-09-14 19:42:37 +0900

    Edit groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
+      redirect_to group_path
     else
       render :new
     end
