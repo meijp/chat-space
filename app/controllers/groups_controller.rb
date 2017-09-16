f3707a1afe2b5b84b7eba4b7aad03c4935c21d24
meijp <mai@mais-MacBook-Pro.local>
2017-09-16 20:19:29 +0900

    Edit groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,6 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    #p @group.user
     if @group.save
       redirect_to root_path, notice: "グループを作成しました"
     else
@@ -21,11 +20,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    renew_params
+    serch_group_params
   end
 
   def update
-    renew_params
+    serch_group_params
     if @group.update(group_params)
       redirect_to root_path, notice: "グループを編集しました"
     else
@@ -38,7 +37,7 @@ class GroupsController < ApplicationController
       params.require(:group).permit(:name, {user_ids: []})
   end
 
-  def renew_params
+  def serch_group_params
       @group = Group.find(params[:id])
   end
 end
